750c044abc0a7508329defd490639cc23bbd779b
AurimasAugutis <aurimasaugutis8@gmail.com>
2020-09-03 00:22:24 +0300

    First commit of University project

---
+++ Assets/AttachHelmet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachHelmet : MonoBehaviour
+{
+    public GameObject helmet;
+    public GameObject hand;
+    public GameObject head;
+
+    public Vector3 offsetHead;
+
+    public Vector3 handOffset = new Vector3(-18.3f, 225.1f, 27f);
+
+    public void AttachToHand()
+    {
+        Debug.Log("here");
+        helmet.transform.parent = hand.transform;
+        //helmet.transform.localPosition = handOffset;
+    }
+
+    public void AttchedToHead()
+    {
+        helmet.transform.parent = head.transform;
+        helmet.transform.localPosition = new Vector3(0f, -41.7f, -13.9f);
+    }
+}
---
+++ Assets/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject helmet;
+    public Quaternion helmRotation;
+
+    GameObject instance;
+
+    private void Awake()
+    {
+        transform.rotation = helmRotation;
+
+        instance = Instantiate(helmet, transform);
+        instance.transform.localScale = new Vector3(0.005f, 0.005f, 0.005f);
+        instance.transform.parent = transform;
+        instance.transform.localPosition = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        transform.rotation = helmRotation;
+    }
+}
